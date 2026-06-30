48b9893b7cf5a3d1e76a9a3015c80b8c95da0ae6
Rajesh Pachhai <pachhairajesh30@gmail.com>
2026-06-30 15:35:24 +0545

    Auth changes

--- myshop/accounts/forms.py
+++ myshop/accounts/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth import get_user_model
 # from .models import CustomUser
 
-User = get_user_model     
+User = get_user_model()    
 
 class RegisterForm(UserCreationForm):
     class Meta:
--- myshop/accounts/urls.py
+++ myshop/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,21 @@ urlpatterns = [
     path('login/', views.log_in, name='login'),
     path('logout/', views.log_out, name='logout'),
     path('password_change/', views.password_change, name='password_change'),
+      # Password reset flow
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), 
+         name='password_reset'),
+
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), 
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), 
+         name='password_reset_complete'),
+
 ]
